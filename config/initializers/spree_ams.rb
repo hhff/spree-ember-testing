3ee11986664bbbf0246363ffbd17e6eadfa5a821
Hugh Francis <me@hughfrancis.me>
2015-04-18 17:12:33 -0400

    Accept requests from all domains

---
+++ config/initializers/spree_ams.rb
@@ -0,0 +1,8 @@
+module Spree
+  module Api
+    Ams.setup do |config|
+      # This list determines which outside domains can communicate with the AMS API.  Use '*' for wildcard.
+      config.cors_whitelist = '*'
+    end
+  end
+end
